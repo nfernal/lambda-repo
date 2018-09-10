28a38f1547e2b1cb57e9ae281b510254cda2c984
Jonathan Simon <nfernal13@outlook.com>
2018-09-09 21:52:06 -0700

    init

--- rubytest/source/main.rb
+++ rubytest/source/main.rb
@@ -8,13 +8,13 @@ puts ARGV[0]
 
 
 ### Example of using Chef/Knife in ruby script
-# require "chef"
-# require 'chef/knife’
+require 'chef'
+require 'chef/knife'
 # require 'chef/knife/bootstrap’
-# require "chef/knife/core/bootstrap_context"
-# require 'chef/knife/ssh’
-# require ‘net/ssh’
-# require ‘net/ssh/multi’
+require 'chef/knife/core/bootstrap_context'
+require 'chef/knife/ssh'
+require 'net/ssh'
+require 'net/ssh/multi'
 # config_file = File.exists?(File.join(Dir.getwd, ‘.chef’, ‘knife.rb’)) ?
 # File.join(Dir.getwd, ‘.chef’, ‘knife.rb’) :
 # File.join(File.expand_path(’~’), ‘.chef’, ‘knife.rb’)
@@ -27,3 +27,28 @@ puts ARGV[0]
 # kb.config[:use_sudo] = true
 # kb.config[:chef_node_name] = name
 # kb.run
+
+
+# def knife_ssh (env, concurrency, chef_path, identity_file, chef_api_key, chef_api_user, name_args, stage)
+#   ssh_user = 'root'
+#   config_file = File.join("#{chef_path}", 'knife', "#{env}", 'knife.rb')
+
+#   knife = Chef::Knife::Ssh.new
+#   knife.name_args = name_args
+
+#   knife.config[:ssh_user] = ssh_user
+#   knife.config[:concurrency] = concurrency
+#   knife.config[:config_file] = config_file
+#   knife.config[:ssh_identity_file] = identity_file
+#   knife.config[:client_key] = chef_api_key
+#   knife.config[:api_key] = chef_api_key
+#   knife.config[:api_client_user] = chef_api_user
+
+#   stdout = StringIO.new
+#   stderr = StringIO.new
+#   knife.ui = Chef::Knife::UI.new(stdout, stderr, STDIN, {})
+#   knife.configure_chef
+#   puts "Running Knife on #{env}! Please Wait...."
+#   knife.run
+#   Chef::Knife::Ssh.reset_config_loader!
+# end
